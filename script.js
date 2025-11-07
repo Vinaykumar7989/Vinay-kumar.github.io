e9f84140d02a917fb73d7b3e2d20dab0281f7740
Vinaykumar7989 <118956424+Vinaykumar7989@users.noreply.github.com>
2025-11-07 21:19:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// This file is currently just a placeholder.
+// You can add functionality here later, like a "back to top" button
+// or filterable projects.
+
+console.log("Portfolio website loaded!");
+
+// Note: Smooth scrolling is handled by 'scroll-behavior: smooth;' in the CSS.
+// We can add more complex JS here if needed.
